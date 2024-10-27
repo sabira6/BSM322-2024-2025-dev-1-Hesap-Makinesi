d117ecad84015d600d00bf1f971ec90821bd60c6
sabira6 <Sabirabarasso97@gmail.com>
2024-10-27 22:38:26 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace Odev1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAddClicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(FirstNumberEntry.Text, out double firstNumber) &&
+                double.TryParse(SecondNumberEntry.Text, out double secondNumber))
+            {
+                double result = firstNumber + secondNumber;
+                ResultLabel.Text = "Sonuç: " + result.ToString();
+            }
+            else
+            {
+                ResultLabel.Text = "Lütfen geçerli sayılar girin.";
+            }
+        }
+
+        private void OnSubtractClicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(FirstNumberEntry.Text, out double firstNumber) &&
+                double.TryParse(SecondNumberEntry.Text, out double secondNumber))
+            {
+                double result = firstNumber - secondNumber;
+                ResultLabel.Text = "Sonuç: " + result.ToString();
+            }
+            else
+            {
+                ResultLabel.Text = "Lütfen geçerli sayılar girin.";
+            }
+        }
+
+        private void OnMultiplyClicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(FirstNumberEntry.Text, out double firstNumber) &&
+                double.TryParse(SecondNumberEntry.Text, out double secondNumber))
+            {
+                double result = firstNumber * secondNumber;
+                ResultLabel.Text = "Sonuç: " + result.ToString();
+            }
+            else
+            {
+                ResultLabel.Text = "Lütfen geçerli sayılar girin.";
+            }
+        }
+
+        private void OnDivideClicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(FirstNumberEntry.Text, out double firstNumber) &&
+                double.TryParse(SecondNumberEntry.Text, out double secondNumber))
+            {
+                if (secondNumber != 0)
+                {
+                    double result = firstNumber / secondNumber;
+                    ResultLabel.Text = "Sonuç: " + result.ToString();
+                }
+                else
+                {
+                    ResultLabel.Text = "Bir sayı sıfıra bölünemez.";
+                }
+            }
+            else
+            {
+                ResultLabel.Text = "Lütfen geçerli sayılar girin.";
+            }
+        }
+    }
+}
